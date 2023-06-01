c61851b8ed25a180207f94fa43d3e3b07f43df05
vishal <vishalgwaghmare31@gmail.com>
2023-06-01 11:38:04 +0530

    basic arraylist added

---
+++ Collection/src/com/collection/ArrayListDemo1.java
@@ -0,0 +1,17 @@
+package com.collection;
+
+import java.util.ArrayList;
+
+public class ArrayListDemo1 {
+	
+	public static void main(String[] args) {
+		ArrayList arrayList= new ArrayList();
+		arrayList.add(10);
+		arrayList.add("java");
+		
+		System.out.println(arrayList);
+		
+		
+	}
+
+}
